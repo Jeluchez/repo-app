822a528d7504784ee0225fc317071317d3e133c2
Jose Luis <jeluchez@gmail.com>
2021-12-17 13:08:34 -0500

    Add check in signup fomr

--- src/components/auth/SignUpForm.js
+++ src/components/auth/SignUpForm.js
@@ -7,9 +7,9 @@ export const SignUpForm = () => {
 
 
     const dispatch = useDispatch();
-    
+
     // const { isloading } = useSelector();
-    const isloading = false 
+    const isloading = false
     const { register, handleSubmit, formState: { errors } } = useForm();
     const [success, setSuccess] = useState();
 
@@ -18,61 +18,84 @@ export const SignUpForm = () => {
     const isSuccessRef = useRef();
 
     const onSubmit = (data) => dispatch(startlogin(data, (success) => {
-        submitRef.current=true;
+        submitRef.current = true;
         isSuccessRef.current = success;
-        isSuccessRef.current=  isSuccessRef.current ? false :true 
+        isSuccessRef.current = isSuccessRef.current ? false : true
     }));
-    
+
     return (
-       /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-       <div className="login__form">
-       {
-           (isSuccessRef.current && submitRef.current) &&
-           <p className="text-danger text-error mb-0 text-center">El correo o el usuario es incorrecto</p>
-       }
-       <form onSubmit={handleSubmit(onSubmit)}>
+        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+        <div className="sign__form">
+            {
+                (isSuccessRef.current && submitRef.current) &&
+                <p className="text-danger text-error mb-0 text-center">El correo o el usuario es incorrecto</p>
+            }
+            <form onSubmit={handleSubmit(onSubmit)}>
 
-           <div className="form-group form-group-email">
-               <label htmlFor="Email" className="email-title">Email Addres</label>
+                <div className="form-group form-group-email">
+                    <label htmlFor="Name" className="email-title">Name</label>
 
-               <input className={errors.email ? 'form-control input-password is-invalid' : 'form-control input-password'} defaultValue="" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
+                    <input className={errors.name ? 'form-control input-password is-invalid' : 'form-control input-password'} defaultValue="" {...register("name", { required: true })} />
 
-               {/*Validation Messages for the Email   */}
+                    {/*Validation Messages for the Username   */}
+                    {errors.name && errors.name.type === "required" && <span className="text-danger text-error">This field is required</span>}
 
-               {errors.email && errors.email.type === "required" && <span className="text-danger text-error">This field is required</span>}
-               {errors.email && errors.email.type === "pattern" && <span className="text-danger text-error">The Email is incorrect</span>}
+                </div>
 
-           </div>
+                <div className="form-group form-group-email">
+                    <label htmlFor="Email" className="email-title">Email Addres</label>
 
-           <div className="form-group form-group-password">
-               <div className="password-title">
-                   <label htmlFor="Password">Password</label>
-                   <a href="#/"><span>Forgot password??</span></a>
-               </div>
+                    <input className={errors.email ? 'form-control input-password is-invalid' : 'form-control input-password'} defaultValue="" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
 
-               <input className={errors.password ? 'form-control input-password is-invalid' : 'form-control input-password'}  {...register("password", { required: true, minLength: 6 })} />
+                    {/*Validation Messages for the Email   */}
 
-               {/*Validation Messages for ther Password  */}
+                    {errors.email && errors.email.type === "required" && <span className="text-danger text-error">This field is required</span>}
+                    {errors.email && errors.email.type === "pattern" && <span className="text-danger text-error">The Email is incorrect</span>}
 
-               {errors.password && errors.password.type === "required" && <span className="text-danger text-error">This field is required</span>}
-               {errors.password && errors.password.type === "minLength" && <span className="text-danger text-error">the length of your password is less than 5 characters</span>}
+                </div>
 
-           </div>
+                <div className="form-group form-group-password">
+                    <div className="password-title">
+                        <label htmlFor="Password">Password</label>
+                        <a href="#/"><span>Forgot password??</span></a>
+                    </div>
 
-           <button className="btn btn-register" /*disabled={isloading}*/>
-               {
-                   isloading
-                       ?
-                       <div className="spinner-border text-light" role="status">
-                           <span className="sr-only"></span>
-                       </div>
-                       :
-                       'Iniciar'
+                    <input className={errors.password ? 'form-control input-password is-invalid' : 'form-control input-password'}  {...register("password", { required: true, minLength: 6 })} />
 
-               }
+                    {/*Validation Messages for ther Password  */}
 
-           </button>
-       </form>
-   </div>
+                    {errors.password && errors.password.type === "required" && <span className="text-danger text-error">This field is required</span>}
+                    {errors.password && errors.password.type === "minLength" && <span className="text-danger text-error">the length of your password is less than 5 characters</span>}
+
+                </div>
+
+                <div className="form-group form-group-check">
+
+                    <input {...register("checkbox", { required: true })} type="checkbox" />
+
+                    <span className='terms'>Creating an account means you’re okay with </span>
+                    <a href='/#'> our Terms & conditions</a>
+
+                    {/*Validation Messages for ther Check  */}
+
+                    {errors.password && errors.password.type === "required" && <span className="text-danger text-error">This field is required</span>}
+
+                </div>
+
+                <button className="btn btn-register" /*disabled={isloading}*/>
+                    {
+                        isloading
+                            ?
+                            <div className="spinner-border text-light" role="status">
+                                <span className="sr-only"></span>
+                            </div>
+                            :
+                            'Iniciar'
+
+                    }
+
+                </button>
+            </form>
+        </div>
     )
 }
